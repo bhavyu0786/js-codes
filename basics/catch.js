ae0df1dd78cd9058253f0a4c090992ff7e6c028f
Bhavy Chaudhary <108673956+bhavyu0786@users.noreply.github.com>
2024-06-11 09:13:51 +0530

    first

---
+++ basics/catch.js
@@ -0,0 +1,29 @@
+// try{
+//   throw new Error("My name is king")
+// }
+// catch(err){
+//   console.log(err.name)
+//   console.log(err.message)
+//   console.log(err.stack)
+// }
+
+// const f = () => {
+    try {
+        let a = 0;
+        console.log(program)
+        // console.log("Program ran successfully")
+        
+}
+catch (err) {
+        // console.log("This is an error")
+        console.log(p)
+}
+finally {
+        console.log("I am a good boyuu")
+        
+}
+ // console.log("I am a good boy")
+// }
+
+// f()
+// console.log("End")
